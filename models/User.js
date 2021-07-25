38910b7925e37c502350d9fd5fe80b193a5324fb
Kimberly Salas <salask0104@gmail.com>
2021-07-24 17:30:35 -0700

    added dep, user models, and server.js

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: {
+            type: Date,
+            default: Date.now,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                //built in ID from mongo
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+
+//virtuals first then exporting process
+// get total count of comments and replies on retrieval
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+
+// create the user model using the userSchema
+const User = model('User', userSchema);
+
+
+// export the user model
+module.exports = User;
+
+
