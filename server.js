23e848c7cfa0ceb38c36614edc8f4e8f145e7efa
Kimberly Salas <salask0104@gmail.com>
2021-08-25 08:42:05 -0700

    edited server.js

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia-api', {
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 // Use this to log mongo queries being executed!
